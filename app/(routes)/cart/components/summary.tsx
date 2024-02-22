ddd9f619648c853327f9e648deb9452d2d19117a
FlareXes <70066527+FlareXes@users.noreply.github.com>
2024-02-22 10:18:19 +0530

    Updated Payment Gateway To RazorPay

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -9,6 +9,24 @@ import { toast } from "react-hot-toast";
 import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
 
+
+
+function loadScript(src) {
+	return new Promise((resolve) => {
+		const script = document.createElement('script')
+		script.src = src
+		script.onload = () => {
+			resolve(true)
+		}
+		script.onerror = () => {
+			resolve(false)
+		}
+		document.body.appendChild(script)
+	})
+}
+
+
+
 const Summary = () => {
 	const searchParams = useSearchParams();
 	const items = useCart((state) => state.items);
@@ -37,8 +55,37 @@ const Summary = () => {
 			}
 		);
 
-		window.location = response.data.url;
-	};
+		const orders = response.data.products;
+		let totalPayment = 0
+		orders.forEach((i: { price: string; }) => {
+			totalPayment += parseInt(i.price)
+
+		});
+
+		const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js')
+
+		if (!res) {
+			alert('Razropay failed to load!!')
+			return
+		}
+
+		var options = {
+			key: "SECRET_KEY",
+			amount: totalPayment * 100, // amount in paise (change accordingly)
+			currency: "INR",
+			name: "Acme Corp",
+			description: "Test Payment",
+			image: "https://example.com/your_logo.png",
+			handler: function (response) {
+				alert("Payment Successful!");
+				console.log(response);
+				window.location.href = "/";
+			},
+		};
+
+		const paymentObject = new window.Razorpay(options);
+		paymentObject.open();
+	}
 
 	return (
 		<div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
